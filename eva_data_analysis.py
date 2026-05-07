e0c60951734df117848077a910733baaef128915
Gaurav Ray <gsray95@proton.me>
2026-05-07 12:16:19 +0100

    added functions and fixed one error

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -4,21 +4,24 @@ import datetime as dt
 import matplotlib.pyplot as plt
 import pandas as pd
 import sys
+import re
 
 def main(input_file, output_file, graph_file):
     print("--START--")
 
-    # read data from JSON file
+    # Read the data from JSON file
     eva_data = read_json_to_dataframe(input_file)
 
-    # convert and export data to csv
+    # Calculate and add crew size to data
+    eva_data = add_crew_size_column(eva_data) # added this line
+
+    # Convert and export data to CSV file
     write_dataframe_to_csv(eva_data, output_file)
 
+    # Sort dataframe by date ready to be plotted (date values are on x-axis)
     eva_data.sort_values('date', inplace=True)
 
-    # calculate eva durations in hours and the cumulative eva time 
-    # and store them in the same dataframe.
-
+    # Plot cumulative time spent in space over years
     plot_cumulative_time_in_space(eva_data, graph_file)
 
     print("--END--")
@@ -82,7 +85,7 @@ def text_to_duration(duration):
         duration_hours (float): The duration in hours
     """
     hours, minutes = duration.split(":")
-    duration_hours = int(hours) + int(minutes)/6  # there is an intentional bug on this line (should divide by 60 not 6)
+    duration_hours = int(hours) + int(minutes)/60  # there is an intentional bug on this line (should divide by 60 not 6)
     return duration_hours
 
 
@@ -102,6 +105,38 @@ def add_duration_hours(df):
     )
     return df_copy
 
+def calculate_crew_size(crew):
+    """
+    Calculate the size of the crew for a single crew entry
+
+    Args:
+        crew (str): The text entry in the crew column containing a list of crew member names
+
+    Returns:
+        (int): The crew size
+    """
+    if crew.split() == []:
+        return None
+    else:
+        return len(re.split(r';', crew))-1
+
+
+def add_crew_size_column(df):
+    """
+    Add crew_size column to the dataset containing the value of the crew size
+
+    Args:
+        df (pd.DataFrame): The input data frame.
+
+    Returns:
+        df_copy (pd.DataFrame): A copy of the dataframe df with the new crew_size variable added
+    """
+    print('Adding crew size variable (crew_size) to dataset')
+    df_copy = df.copy()
+    df_copy["crew_size"] = df_copy["crew"].apply(
+        calculate_crew_size
+    )
+    return df_copy
 
 if __name__ == "__main__":
     # Data source: https://data.nasa.gov/resource/eva.json (with modifications)
